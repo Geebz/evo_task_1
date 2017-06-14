d6c3551d809707fb8d8171513966868274a2b049
Oleksandr Veklych <alexandrveklich@gmail.com>
2017-06-14 15:33:02 +0300

    update tests, add interactive mode

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from pprint import pprint
+import re
 
 
 def update(data, service, count):
-    pass
+    while count:
+        min_number_of_instances_service = min(data, key=lambda x: sum(data[x].values()))
+        if service in data[min_number_of_instances_service]:
+            data[min_number_of_instances_service][service] += 1
+        else:
+            data[min_number_of_instances_service][service] = 1
+        count -= 1
+    return data
 
 
 def main():
@@ -16,13 +24,39 @@ def main():
         },
     }
 
+    print("Hello Evo! Welcome to program.")
     print("Configuration before:")
     pprint(example_data)
 
-    update(example_data, 'pylons', 7)
+    while True:
+        while True:
+            service = input("What service do you want to add?: ").lower()
+            if re.fullmatch('^[a-z]+$', service) is None:
+                print("Service must be written only letters")
+                continue
+            else:
+                break
+
+        while True:
+            try:
+                number = int(input("How many instances do you want to run?: "))
+                break
+            except ValueError:
+                print("Number must be integer. Let`s try again")
+                continue
+
+        update(example_data, service, number)
+
+        decision = input("Do you want to add another one? Yes or No: ")
+        if decision.lower() == 'yes':
+            continue
+        else:
+            break
 
     print("Configuration after:")
     pprint(example_data)
+    print("That`s it. Let`s go to another task ;)")
+
 
 if __name__ == '__main__':
     main()
--- test_update.py
+++ test_update.py
@@ -3,7 +3,7 @@ import pytest
 import main
 
 
-def test_update():
+def test_update_1():
     config = {
         'ginger': {
             'django': 2,
@@ -14,7 +14,8 @@ def test_update():
         },
     }
 
-    main.update(config, 'pylons', 7)
+    main.update(config, 'pylons', 6)
+
     assert config == {
         'ginger': {
             'django': 2,
@@ -23,7 +24,45 @@ def test_update():
         },
         'cucumber': {
             'flask': 1,
-            'pylons': 6,
+            'pylons': 5,
+        },
+    }
+
+
+def test_update_2():
+    config = {
+        'ginger': {
+            'django': 5,
+            'flask': 3,
+        },
+        'cucumber': {
+            'flask': 4,
+        },
+        'nginx': {
+            'ruby': 2,
+        },
+        'gunicorn': {
+            'tornado': 2,
+        },
+    }
+
+    main.update(config, 'pylons', 2)
+
+    assert config == {
+        'ginger': {
+            'django': 5,
+            'flask': 3,
+        },
+        'cucumber': {
+            'flask': 4,
+        },
+        'nginx': {
+            'ruby': 2,
+            'pylons': 1
+        },
+        'gunicorn': {
+            'tornado': 2,
+            'pylons': 1
         },
     }
 
@@ -38,26 +77,26 @@ def test_initial():
     main.update(config, 'django', 3)
 
     assert sum(config['ginger'].values()) == sum(config['cucumber'].values())
-    assert sum(sum(x.values()) for x in config.values()) == 3+3
+    assert sum(sum(x.values()) for x in config.values()) == 3 + 3
 
 
-@pytest.mark.xfail(reason="Advanced test. Optional to implement")
-def test_predictable_config():
-    permutations = []
-    services = [
-        ('flask', 7),
-        ('django', 13),
-        ('pylons', 17)
-    ]
-
-    for permutation in itertools.permutations(services):
-        config = {
-            'ginger': {},
-            'cucumber': {},
-        }
-        for svc, num in permutation:
-            main.update(config, svc, num)
-        assert sum(sum(x.values()) for x in config.values()) == 7+13+17
-        permutations.append(config)
-
-    assert all(p == permutations[0] for p in permutations[1:])
+# @pytest.mark.xfail(reason="Advanced test. Optional to implement")
+# def test_predictable_config():
+#     permutations = []
+#     services = [
+#         ('flask', 7),
+#         ('django', 13),
+#         ('pylons', 17)
+#     ]
+#
+#     for permutation in itertools.permutations(services):
+#         config = {
+#             'ginger': {},
+#             'cucumber': {},
+#         }
+#         for svc, num in permutation:
+#             main.update(config, svc, num)
+#         assert sum(sum(x.values()) for x in config.values()) == 7 + 13 + 17
+#         permutations.append(config)
+#
+#     assert all(p == permutations[0] for p in permutations[1:])
